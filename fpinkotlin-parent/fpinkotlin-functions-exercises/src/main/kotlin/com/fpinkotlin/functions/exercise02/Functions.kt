b929274ca1ffb061991904edb4c17122bca7e83b
Robertas Setkus <robertas.setkus@ee.co.uk>
2019-07-22 13:48:54 +0100

    gradlew wrapper

--- fpinkotlin-parent/fpinkotlin-functions-exercises/src/main/kotlin/com/fpinkotlin/functions/exercise02/Functions.kt
+++ fpinkotlin-parent/fpinkotlin-functions-exercises/src/main/kotlin/com/fpinkotlin/functions/exercise02/Functions.kt
@@ -6,3 +6,4 @@ fun square(n: Int) = n * n
 fun triple(n: Int) = n * 3
 
 // Define a generic compose. The exercise dos not consist in writing the implementation, but the signature.
+fun <T, T1, R> compose(f: (T) -> R, g: (T1) -> T): (T1) -> R = { f(g(it)) }
